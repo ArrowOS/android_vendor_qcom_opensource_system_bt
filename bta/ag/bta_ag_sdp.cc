GrapheneOS/platform_packages_modules_Bluetooth

SDP: Only start discovery once
Bug: 174052148
Test: pair with headphones
Tag: #security
Change-Id: I1d014a7b793bb1b66e26652f6696499ea36a6510

system/bta/ag/bta_ag_sdp.cc
@@ -34,6 +34,7 @@
 #include "bta_sys.h"
 #include "btif_config.h"
 #include "btm_api.h"
+#include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "sdp_api.h"
 #include "stack/btm/btm_sec.h"
@@ -477,6 +478,12 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
       /* Legacy from HSP v1.0 */
       uuid_list[0] = Uuid::From16Bit(UUID_SERVCLASS_HEADSET);
     }
+  }
+
+  if (p_scb->p_disc_db != nullptr) {
+    android_errorWriteLog(0x534e4554, "174052148");
+    LOG_ERROR("Discovery already in progress... returning.");
+    return;
   }
 
   /* allocate buffer for sdp database */

ArrowOS/android_vendor_qcom_opensource_system_bt

(cherry picked from commit cfa5a74ea90a09e1c7413a25f04332ee2d1e3f21)
Merged-In: I1d014a7b793bb1b66e26652f6696499ea36a6510
(cherry picked from commit f0994f6e4723eddaa617b68139f064d945d9389e)

bta/ag/bta_ag_sdp.cc
@@ -33,6 +33,7 @@
 #include "bta_api.h"
 #include "bta_sys.h"
 #include "btm_api.h"
+#include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "sdp_api.h"
 #include "utl.h"
@@ -450,6 +451,12 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
     } else {
       uuid_list[0] = Uuid::From16Bit(UUID_SERVCLASS_HEADSET);
     }
+  }
+
+  if (p_scb->p_disc_db != nullptr) {
+    android_errorWriteLog(0x534e4554, "174052148");
+    APPL_TRACE_ERROR("Discovery already in progress... returning.");
+    return;
   }
 
   /* allocate buffer for sdp database */